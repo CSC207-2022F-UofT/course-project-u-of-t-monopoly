d00728df2a19d96e5e0f25486bd7814992a1a757
adamjvalentini <adam.valentini@mail.utoronto.ca>
2022-11-14 11:58:33 -0500

    Updated the BoardGame entity class to include the associated methods and data as outlined in the CRC cards and implementation plan. Intial Test case are completed with more still needed once the other entities are updated

---
+++ src/main/java/Entity/GameBoard.java
@@ -0,0 +1,102 @@
+package Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameBoard {
+    public List<Integer> players;
+    public Integer[] board = new Integer[40];
+    //private int goPosition = 0;
+    //private int jailPosition = 30;
+    //private int[] chanceCard = new int[]{7, 22, 36};
+    //  private int[] oppurtunityCard = new int[]{2, 17, 33};
+    static int turn = 0;
+
+    public GameBoard(){
+
+    }
+
+
+    // monopoly overall class that will keep game organized
+
+    public GameBoard(Player inputPlayers, Properties[] lstProperties){
+        this.players = inputPlayers;
+        this.board = lstProperties;
+
+    }
+
+    public void addPlayer(PLayer player1){
+        players.add(player2);
+    }
+
+
+    public ArrayList<Integer> rollDice(){
+        ArrayList<Integer> rolls = new ArrayList<Integer>();
+        Random num = new Random();
+        int max = 6;
+        int min = 1;
+        int randnum = num.nextInt((max - min) + 1) + min;
+        int randnum2 = num.nextInt((max-min) + 1) + min;
+        rolls.add(randnum);
+        rolls.add(randnum2);
+        //Or return the sum of the rolls
+        //int sum = randnum + randnum2;
+        //return sum;
+        return rolls;
+    }
+
+    public int getNumPlayers(){
+        return players.size();
+
+    }
+
+    public Properties getTile(int index){
+        return board[index];
+    }
+
+    public int getTileIndex(Properties place){
+        for (int i = 0; i <board.length; i++){
+            if (place == board[i]){
+                return i;
+            }
+        }
+    }
+
+    public void movePlayer(Player player1, int movement){
+        player1.location = (player1.location + movement)%40;
+        //update player posisiton with index
+        //how are we keeping track of player location
+
+    }
+
+    public boolean checkGameEnd(){
+        // what is the game end
+        int numPlayers = players.size();
+        int count = 0;
+
+        for (Player person : players){
+            if (person. checkBankruptcy()){
+                count += 1;
+
+            }
+            if (count == (numPlayers - 1)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public Player nextTurn(){
+        if (turn == 0){
+            turn +=1;
+        }
+        for (Player person : players){
+            if (person.id == turn % getNumPlayers()){
+                return person;
+                turn += 1;
+            }
+        }
+    }
+}
