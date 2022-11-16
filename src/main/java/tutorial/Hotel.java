ddaf06cd51182cf0b9a85db996c3ad356994169d
JTQ <tqjimmy.jin@mail.utoronto.ca>
2022-11-15 21:02:14 -0500

    Entities: Properties class; Abstract class Building; House, Hotel extends Building; Test: PropertiesTest

---
+++ src/main/java/tutorial/Hotel.java
@@ -0,0 +1,10 @@
+package tutorial;
+
+public class Hotel extends Building{
+
+    public Hotel(Integer price, Integer rent, Integer mortgageValue){
+        super(price, rent, mortgageValue);
+    }
+
+
+}
---
+++ src/main/java/tutorial/House.java
@@ -0,0 +1,10 @@
+package tutorial;
+
+public class House extends Building{
+
+    public House(Integer price, Integer rent, Integer mortgageValue){
+        super(price, rent, mortgageValue);
+    }
+
+
+}
