1fd32810e0d438dfdbc2ad3cc13700e6b066f2b7
Johnny H <84548171+JohnnyH-Dev@users.noreply.github.com>
2022-11-24 13:00:38 -0500

    Update Player.java

--- src/main/java/tutorial/Player.java
+++ src/main/java/tutorial/Player.java
@@ -1,74 +1,76 @@
 package tutorial;
 
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Player {
 
-
     String username; // name of the player
     int money;
-
     int playerPosition;
     String opportunityCards;
-
-    boolean inJail;
-
     ArrayList<Building> buildings = new ArrayList<Building>();
 
-    ArrayList<Utilities> utilities = new ArrayList<Utilities>();
+    Jail jail;
 
-    ArrayList<Railroad> railroads = new ArrayList<Railroad>();
+    int id;
 
-    public Player(String username,int money,int position,String opportunityCards){
+    public Player(String username, int money, int position, String opportunityCards, int id) {
         this.username = username;
         this.money = money;
         this.playerPosition = position;
         this.opportunityCards = opportunityCards;
+        this.id = id;
     }
-    public String getUsername(){
+
+    public String getUsername() {
         return this.username;
     }
 
-    public int getMoney(){
+    public int getMoney() {
         return this.money;
     }
 
-    public int loseMoney(int amount){
-        if(this.money >= amount){
+    public int loseMoney(int amount) {
+        if (this.money >= amount) {
             this.money -= amount;
-        }
-        else{
+        } else {
             this.money = 0;
         }
         return amount;
     }
 
-    public int getPlayerPosition(){
+    public ArrayList<Integer> rollDice(){
+        ArrayList<Integer> rolls = new ArrayList<Integer>();
+        Random num = new Random();
+        int max = 6;
+        int min = 1;
+        int randnum = num.nextInt((max - min) + 1) + min;
+        int randnum2 = num.nextInt((max-min) + 1) + min;
+        rolls.add(randnum);
+        rolls.add(randnum2);
+        //Or return the sum of the rolls
+        //int sum = randnum + randnum2;
+        //return sum;
+        return rolls;
+    }
+
+    public int getPlayerPosition() {
         return this.playerPosition;
     }
 
-    public void addMoney(int amount){
+    public void addMoney(int amount) {
         this.money += amount;
     }
 
-    public void addProperty(Building property){
+    public void addProperty(Building property) {
         buildings.add(property);
     }
-    public void addProperty(Utilities property){
-        utilities.add(property);
+
+    public boolean checkBankruptcy() {
+        if (this.money == 0) {
+            return true;
+        }
+        return false;
     }
-    public void addProperty(Railroad property){
-        railroads.add(property);
-    }
-    public void inJail(boolean trueorfalse) {
-        this.inJail = trueorfalse;
-    }
-
-
-
-
-
-
-
-
-
 }
