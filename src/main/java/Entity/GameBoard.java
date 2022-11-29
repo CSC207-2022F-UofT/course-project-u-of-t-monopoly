c5b0b84a10cf14dbbe6dfb8b6053e8c7ddeeba21
adamjvalentini <113124536+adamjvalentini@users.noreply.github.com>
2022-11-29 13:27:05 -0500

    Update GameBoard.java
    Added Javadocs

--- src/main/java/Entity/GameBoard.java
+++ src/main/java/Entity/GameBoard.java
@@ -7,7 +7,7 @@ import java.util.Random;
 // The class has methods to find property tiles, by name and index
 // The class can return both the board and players list
 // The class can move players, add players, and set the next turn (to a player)
-// The class has the functionality to check the game end. 
+// The class has the functionality to check the game end.
 public class GameBoard {
     private List<Player> players;
     private Properties[] board = new Properties[40];
@@ -24,32 +24,65 @@ public class GameBoard {
 
     // monopoly overall class that will keep game organized
 
+    /**
+     * Create a new GameBoard Object based on the inputted list of players and list of properties
+     *
+     * @param inputPlayers
+     * @param lstProperties
+     */
+
     public GameBoard(List<Player> inputPlayers, Properties[] lstProperties){
         this.players = inputPlayers;
         this.board = lstProperties;
-
     }
 
+    /**
+     * Add a player to the player list associated with this GameBoard
+     * @param player1
+     */
     public void addPlayer(Player player1){
         players.add(player1);
     }
-    
+
+    /**
+     * Return the list of players asscociated with this GameBoard
+     * @return this.players
+     */
     public List<Player> getPlayers(){
         return this.players;
     }
-    
+
+    /**
+     * Return the list of properties associated with this GameBoard
+     * @return this.properties
+     */
     public Properties[] getProperties(){
         return this.board;
     }
-   
+
+    /**
+     * Return the number of players currenly playing with the GameBoard
+     * @return int of number of players
+     */
     public int getNumPlayers(){
         return players.size();
     }
 
+    /**
+     * using the index, return the properties object at the index.
+     *
+     * @param index
+     * @return a properties object given the index
+     */
     public Properties getTile(int index){
         return board[index];
     }
 
+    /**
+     * Given the inputted Properties object, return the index that the property is at. An int from 0-39
+     * @param place
+     * @return the int at which place is located
+     */
     public int getTileIndex(Properties place){
         for (int i = 0; i <board.length; i++){
             if (place == board[i]){
@@ -58,13 +91,22 @@ public class GameBoard {
         }
     }
 
+    /**
+     * Given the inputted movement, move player along the board (using the player.playerPosition variable
+     * @param player1
+     * @param movement
+     * 
+     */
     public void movePlayer(Player player1, int movement){
         player1.playerPosition = (player1.playerPosition + movement)%40;
         //update player posisiton with index
         //how are we keeping track of player location
-
     }
 
+    /**
+     * Check the game end based on the number of players that are bankrupt relative ot the total number of players
+     * @return a boolean to whether this game has ended
+     */
     public boolean checkGameEnd(){
         // what is the game end
         int numPlayers = players.size();
@@ -83,6 +125,11 @@ public class GameBoard {
     }
 
 
+    /**
+     * Return the player object who has the current turn
+     * 
+     * @return retrun the player object who has the current turn
+     */
     public Player nextTurn(){
         if (turn == 0){
             turn +=1;
