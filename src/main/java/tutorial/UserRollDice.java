1b0f20ee7a390ad6b160dfe77dc95221777da0b5
pujiacheng <jiacheng.pu@mail.utoronto.ca>
2022-11-28 20:23:03 -0500

    need to add special cases(when landing on jails, community chest, or chance tile.)

--- src/main/java/tutorial/UserRollDice.java
+++ src/main/java/tutorial/UserRollDice.java
@@ -4,13 +4,27 @@ import java.util.ArrayList;
 
 public class UserRollDice {
 
+
     Player player;
     Gameboard gameboard;
+
+    /**
+     * This class will be used when one player roll a die and make movement.
+     *
+     * @param player The player who rolls the die and reacts.
+     * @param gameboard The game board.
+     */
 
     public UserRollDice(Player player, Gameboard gameboard) {
         this.player = player;
         this.gameboard = gameboard;
     }
+
+    /**
+     * This method is used to roll a die and make corresponding changes.
+     *
+     * @return the corresponding message shown to player.
+     */
 
     public String react() {
         Player player = this.player;
@@ -24,7 +38,11 @@ public class UserRollDice {
         if (properties.getOwner() == null) {
             // player lands on an unowned properties.
             return userPresenter.prepareView("Would you like to buy this " + properties.getName() + " ?");
-        } else if (gameboard.players.contains(properties.getOwner())) {
+        } else if (properties.getOwner() == player) {
+            // the land is owned by the player.
+            return userPresenter.prepareView("Would you like to build a house or a hotel to " +
+                    "raise the value of the property?");
+        } else (properties.OwnedByPlayer()) {
             // player lands on a properties owned by other player and the player should pay the rent.
             if (player.getMoney() >= properties.getRent()) {
                 player.loseMoney(properties.getRent());
@@ -34,21 +52,18 @@ public class UserRollDice {
                         properties.getRent() + "\n " + properties.getOwner().getUsername()
                         + " gets " + properties.getRent());
             } else {
-                player.money = 0;
-                return userPresenter.prepareView(player.getUsername() + " losses all money.");
+                return userPresenter.prepareView(player.getUsername() + " does not have enough money" + "\n" +
+                        "Sell or mortgage your properties");
             }
-        } else {
-            // player lands on utilities or a jail, use other use case interactor.
-           if (properties.isinstanceof(Utilities)) {
-               UserInteracttoUtilities userInteracttoUtilities = new UserInteracttoUtilities(player, properties);
-               return userInteracttoUtilities.react();
-           } else if (properties.isinstanceof(Jail)) {
-               UserInteracttoJail userInteracttoJail = new UserInteracttoJail(player, preperties);
-               return userInteracttoJail.react();
-           } else {return userPresenter.prepareView("Error");}
-        }
+        } //else {
+            // player lands on a jail, use other use case interactor.
+           //if (properties.isinstanceof(Jail)) {
+           //    UserInteracttoJail userInteracttoJail = new UserInteracttoJail(player, preperties);
+           //    return userInteracttoJail.react();
+           //} else {return userPresenter.prepareView("Error");}
+        //}
     }
 
-
+// should add cases of landing on jails, community chest or chance tile.
 
 }
