b71a126e240de3e0f8ebe659dcd5b9632fa86e1e
pujiacheng <jiacheng.pu@mail.utoronto.ca>
2022-11-29 19:39:39 -0500

    add java doc for community card and chance card.

--- src/main/java/tutorial/ChanceCard.java
+++ src/main/java/tutorial/ChanceCard.java
@@ -26,6 +26,12 @@ public class ChanceCard extends OpportunityCard {
     //return the card at index of the random int
     //}
 
+    /**
+     * Construct a Chance Card
+     * @param cardname the name of the card
+     * @param moneyamount amount of money changed for the player
+     * @param displacement movement for the player.
+     */
     public ChanceCard(String cardname, int moneyamount, int displacement) {
         super();
         this.cardname = cardname;
@@ -33,14 +39,26 @@ public class ChanceCard extends OpportunityCard {
         this.movement = displacement;
     }
 
+    /**
+     * show the card name.
+     * @return card name
+     */
     public String getCard() {
         return this.cardname;
     }
 
+    /**
+     * show amount of money changed
+     * @return amount of money changed
+     */
     public int getMoneychange() {
         return this.moneychange;
     }
 
+    /**
+     * show the movement
+     * @return place movement.
+     */
     public int getMovement() {
         return this.movement;
     }
