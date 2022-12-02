91d22f1d1a7dc9cc216b588fcd91fb61224ebe86
Damngoblin <113082291+Damngoblin@users.noreply.github.com>
2022-12-02 14:48:28 -0500

    Update CommunityCard.java

--- src/main/java/Entity/CommunityCard.java
+++ src/main/java/Entity/CommunityCard.java
@@ -9,7 +9,12 @@ public class CommunityCard extends OpportunityCard{
     String cardname;
 
 
-
+    /**
+     * Construct a community card class
+     * @param cardname The name of this community card.
+     * @param moneyamount Amount of money changed for the player.
+     * @param displacement Movement for the player.
+     */
     public CommunityCard(String cardname, int moneyamount, int displacement) {
         super();
         this.cardname = cardname;
@@ -17,9 +22,21 @@ public class CommunityCard extends OpportunityCard{
         this.movement = displacement;
     }
 
+     /**
+     * Show the community card name.
+     * @return the name of the card.
+     */
     public String getCard() {return this.cardname;}
 
+     /**
+     * Show amount of money changed for the player.
+     * @return amount of money changed
+     */
     public int getMoneychange() {return this.moneychange;}
-
+    
+    /**
+     * Show the movement for the player.
+     * @return the movement for the player
+     */
     public int getMovement() {return this.movement;}
 }
