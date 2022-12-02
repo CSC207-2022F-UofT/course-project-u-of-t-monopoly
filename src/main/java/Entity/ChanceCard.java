d3853c03842bc2042db0fe3801c1a4a55ca35d32
Damngoblin <113082291+Damngoblin@users.noreply.github.com>
2022-12-02 14:45:27 -0500

    Update ChanceCard.java

--- src/main/java/Entity/ChanceCard.java
+++ src/main/java/Entity/ChanceCard.java
@@ -26,21 +26,44 @@ public class ChanceCard extends OpportunityCard {
     //return the card at index of the random int
     //}
 
+    
+    /**
+     * Construct a Chance Card
+     *
+     * @param cardname     the name of the card
+     * @param moneyamount  amount of money changed for the player
+     * @param displacement movement for the player.
+     */
     public ChanceCard(String cardname, int moneyamount, int displacement) {
         super();
         this.cardname = cardname;
         this.moneychange = moneyamount;
         this.movement = displacement;
     }
-
+    
+    /**
+     * show the card name.
+     *
+     * @return card name
+     */
     public String getCard() {
         return this.cardname;
     }
 
+    /**
+     * show amount of money changed
+     *
+     * @return amount of money changed
+     */
     public int getMoneychange() {
         return this.moneychange;
     }
 
+    /**
+     * show the movement
+     *
+     * @return place movement.
+     */
     public int getMovement() {
         return this.movement;
     }
