a6cd8cfcecf5e7bd617b5c2091fea0bba942fac9
Damngoblin <113082291+Damngoblin@users.noreply.github.com>
2022-12-04 20:27:27 -0500

    Create BuildProcessor.java

---
+++ src/main/java/UseCase/BuildProcessor.java
@@ -0,0 +1,51 @@
+package UseCase;
+
+import Entity.*;
+
+import java.util.Dictionary;
+
+public class BuildProcessor {
+    private Player player;
+    private Properties properties;
+
+    private GameBoard gameBoard;
+
+    public BuildProcessor(Player player, Properties properties, GameBoard gameBoard) {
+        // controller pass an object of Player and an object of Properties
+        this.player = player;
+        this.properties = properties;
+        this.gameBoard = gameBoard;
+    }
+
+    public boolean Build(House house) {
+        // controller calls HouseHotelGenerator to generate a house
+        if (this.BuildChecker(house)){
+            this.player.loseMoney(house.getPrice());
+            this.properties.buildBuilding(house);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean Build(Hotel hotel) {
+        // controller calls HouseHotelGenerator to generate a house
+        if (this.BuildChecker(hotel)){
+            this.player.loseMoney(hotel.getPrice());
+            // since when hotel is built, all houses are removed.
+            this.properties.removeBuilding();
+            this.properties.buildBuilding(hotel);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean BuildChecker(Building building) {
+        // helper function to avoid duplicated code
+        if (this.player.getMoney() >= building.getPrice()){
+            return this.properties.getOwner() == this.player;
+        }
+        return false;
+    }
+
+}
+
