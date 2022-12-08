e4650a6291449178832b55e54102df523694b1ab
adamjvalentini <113124536+adamjvalentini@users.noreply.github.com>
2022-12-08 10:42:38 -0500

    Update HouseHotelGenerator.java
    Added Javadocs

--- src/main/java/UseCase/HouseHotelGenerator.java
+++ src/main/java/UseCase/HouseHotelGenerator.java
@@ -3,14 +3,32 @@ package UseCase;
 import Entity.Hotel;
 import Entity.House;
 
+/**
+ * Class to build Hotel and House objects for the purpose of adding them to a 
+ * Properties object 
+ */
 public class HouseHotelGenerator {
     // this class is to solve hard dependency issue (this can be tested independently)
     // when controller calls BuildProcessor, it should also call this generator as well
 
+    /**
+     * Build a House object 
+     * @param price the price of the house 
+     * @param rent the rent associated with the house 
+     * @param mortgageValue the mortgage value of the house object 
+     * @return the House object 
+     */
     public House generateHouse(Integer price, Integer rent, Integer mortgageValue) {
         return new House(price,rent,mortgageValue);
     }
 
+    /**
+     * Build a Hotel object 
+     * @param price the price of the Hotel 
+     * @param rent the rent of the Hotel 
+     * @param mortgageValue the mortgageValue of the Hotel 
+     * @return the newly built Hotel Object
+     */
     public Hotel generateHotel(Integer price, Integer rent, Integer mortgageValue) {
         return new Hotel(price, rent, mortgageValue);
     }
