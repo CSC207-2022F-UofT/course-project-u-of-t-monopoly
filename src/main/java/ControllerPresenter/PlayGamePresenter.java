79096ee1a4f0ae3dc5c5c7fdaa950449ce3575a5
Damngoblin <113082291+Damngoblin@users.noreply.github.com>
2022-12-08 17:53:19 -0500

    Add files via upload

---
+++ src/main/java/ControllerPresenter/PlayGamePresenter.java
@@ -0,0 +1,314 @@
+package ControllerPresenter;
+import Entity.Jail;
+import Entity.TileCanBuy;
+import UseCase.UserRollDice;
+
+import javax.management.remote.JMXConnectorFactory;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class PlayGamePresenter {
+
+
+    public void RollDice(UserRollDice urd, ArrayList<Integer> rolls, String name) {
+        JFrame f = new JFrame("Roll Dice");
+        JLabel l = new JLabel("Now its " + name + "'s turn, Please roll dice" );
+        JButton roll = new JButton("Roll");
+        f.setSize(500, 400);
+        l.setBounds(100, 100, 300, 80);
+        roll.setBounds(150, 200, 200, 100);
+        roll.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                ArrayList<Integer> lst = urd.react();
+
+
+
+                JFrame frame = new JFrame("Result");
+                frame.setSize(400, 300);
+                JLabel result = new JLabel(lst.get(0) + " , " + lst.get(1));
+                result.setBounds(125, 100, 50, 100);
+                frame.add(result);
+                frame.setLayout(null);
+                frame.setVisible(true);
+
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                JButton ok = new JButton("ok");
+                ok.setBounds(175, 200, 50, 50);
+                frame.add(ok);
+                ok.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        frame.dispose();
+                    }
+                });
+
+                rolls.set(0, lst.get(0));
+                rolls.set(1, lst.get(1));
+
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                f.dispose();
+
+            }
+        });
+        f.add(l);
+        f.add(roll);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void BuildOption(ArrayList<Object> build){
+        JFrame f=new JFrame("Building options");
+        JButton b1=new JButton("Yes");
+        JButton b2=new JButton("No");
+        JLabel l=new JLabel("Would you like to build a house/hotel?");
+        b1.setBounds(50,150,95,30);
+        b2.setBounds(200,150,95, 30);
+        l.setBounds(60,70,300,30);
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                build.set(0, true);
+                f.dispose();
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                build.set(0, false);
+                f.dispose();
+            }
+        });
+        f.add(b1);
+        f.add(b2);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void BuyProperty(ArrayList<Object> buy, String name){
+        JFrame f=new JFrame("Buy property");
+        JButton b1=new JButton("Yes");
+        JButton b2=new JButton("No");
+        JLabel l=new JLabel("Would you like to buy " + name);
+        b1.setBounds(50,150,95,30);
+        b2.setBounds(200,150,95, 30);
+        l.setBounds(60,70,300,30);
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                buy.set(0, true);
+                f.dispose();
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                buy.set(0, false);
+                f.dispose();
+            }
+        });
+        f.add(b1);
+        f.add(b2);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void BuyPropertySuccessful(String name) {
+        JFrame f=new JFrame("Buy property succeed");
+        JLabel l=new JLabel("Purchase succeed, now you have " + name + " .");
+        l.setBounds(60,70,300,30);
+        JButton ok = new JButton("OK");
+        ok.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                f.dispose();
+            }
+        });
+        f.add(l);
+        f.add(ok);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void NotEnoughMoney() {
+        JFrame f=new JFrame("Unsuccessful");
+        JLabel l=new JLabel("You don't have enough money");
+        l.setBounds(60,70,300,30);
+        JButton ok = new JButton("OK");
+        ok.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                f.dispose();
+            }
+        });
+        f.add(l);
+        f.add(ok);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void payRent(Integer amount, String tile, boolean mortgaged, String name){
+        JFrame f=new JFrame("Pay rent");
+        JLabel l;
+        if (!mortgaged) {
+            l = new JLabel("You landed on " + tile + " ," + amount + " dollars are paid");
+        }
+        else{
+            l = new JLabel("You landed on " + tile + " ," + name + " is mortgaged to avoid bankruptcy");
+        }
+        l.setBounds(60,70,300,30);
+
+
+        f.add(l);
+
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+
+    public void Bankrupt(){
+        JFrame f=new JFrame("Bankrupt Notice");
+        JLabel l=new JLabel("Sorry, you're broke");
+        l.setBounds(120,120,300,30);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void Mortgage(String name){
+        JFrame f=new JFrame("Mortgage Notice");
+        JLabel l=new JLabel("You mortgaged " + name + " to avoid bankruptcy");
+        l.setBounds(120,120,300,30);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void UseJailFreeCard(ArrayList<Object> use){
+        JFrame f=new JFrame("Use jail free card");
+        JButton b1=new JButton("Yes");
+        JButton b2=new JButton("No");
+        JLabel l=new JLabel("Would you like to use your jail free card");
+        b1.setBounds(50,150,95,30);
+        b2.setBounds(200,150,95, 30);
+        l.setBounds(60,70,300,30);
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                use.set(0, true);
+                f.dispose();
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                use.set(0, false);
+                f.dispose();
+            }
+        });
+        f.add(b1);
+        f.add(b2);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void BuyUtility(ArrayList<Object> utility){
+        JFrame f=new JFrame("Buy Utility");
+        JButton b1=new JButton("Yes");
+        JButton b2=new JButton("No");
+        JLabel l=new JLabel("Would you like to buy the utility");
+        b1.setBounds(50,150,95,30);
+        b2.setBounds(200,150,95, 30);
+        l.setBounds(60,70,300,30);
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                utility.set(0, true);
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                utility.set(0, false);
+            }
+        });
+        f.add(b1);
+        f.add(b2);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void getOutofJailFreeCard(){
+        JFrame f=new JFrame("Opportunity Card");
+        JLabel l=new JLabel("Congrats! You get a jail free card");
+        l.setBounds(120,120,300,30);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void TaxPlayer(Integer amount) {
+        JFrame f = new JFrame("Taxed");
+        JLabel l = new JLabel("You have been taxed for " + amount + " dollars");
+        JButton ok = new JButton("OK");
+        f.setSize(400, 300);
+        l.setBounds(100, 200, 200, 80);
+        ok.setBounds(180, 300, 40, 40);
+        ok.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                f.dispose();
+            }
+        });
+
+        f.add(l);
+        f.add(ok);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void PlayerGoToJail(){
+        JFrame f=new JFrame("Go to jail");
+        JLabel l=new JLabel("So sad, you are send to jail");
+        l.setBounds(120,120,300,30);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void CommunityCardPresenter(String message){
+        JFrame f=new JFrame("Received community card");
+        JLabel l=new JLabel(message);
+        l.setBounds(120,120,300,30);
+        f.add(l);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
---
+++ src/main/java/ControllerPresenter/TileHandler.java
@@ -0,0 +1,165 @@
+package ControllerPresenter;
+
+import Entity.Properties;
+import Entity.RailRoad;
+import Entity.TileCanBuy;
+import Entity.Utilities;
+import UseCase.*;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class TileHandler {
+
+    private MovePlayer movePlayer;
+    private PlayGamePresenter playGamePresenter;
+
+    private PlayerGoToJailProcessor playerGoToJailProcessor;
+
+    //private UserPayRent userPayRent;
+
+    //private BuyPropertyProcessor buyPropertyProcessor;
+
+    //private UserBuyUtilities userBuyUtilities;
+
+    private DeckDealToPlayer deckDealToPlayer;
+    private UserTileInteractor userTileInteractor;
+    private ArrayList<Integer> rolls;
+
+
+    public TileHandler(MovePlayer movePlayer, PlayGamePresenter playGamePresenter, PlayerGoToJailProcessor playerGoToJailProcessor,
+                       ArrayList<Integer> rolls,DeckDealToPlayer deckDealToPlayer, UserTileInteractor userTileInteractor){
+        this.movePlayer = movePlayer;
+        this.playGamePresenter = playGamePresenter;
+        this.playerGoToJailProcessor = playerGoToJailProcessor;
+        //this.userPayRent = userPayRent;
+        //this.buyPropertyProcessor = buyPropertyProcessor;
+        this.deckDealToPlayer = deckDealToPlayer;
+        //this.userBuyUtilities = userBuyUtilities;
+        this.rolls = rolls;
+        this.userTileInteractor = userTileInteractor;
+    }
+    public Object HandleTile(Object tile) {
+        if (tile instanceof String) {
+            // may be simple factory design pattern
+            if (tile == "Income Tax"){
+                String message = this.movePlayer.taxPlayer(200);
+                if (Objects.equals(message, "Success")){
+                    this.playGamePresenter.TaxPlayer(200);
+                    return null;
+                }
+                if (Objects.equals(message, "Bankrupt")){
+                    this.playGamePresenter.Bankrupt();
+                    return "Bankrupt";
+                }
+                else{
+                    this.playGamePresenter.Mortgage(message);
+                    return null;
+                }
+
+            }
+            else if (tile == "Luxury Tax") {
+                String message = this.movePlayer.taxPlayer(100);
+                if (Objects.equals(message, "Success")){
+                    this.playGamePresenter.TaxPlayer(100);
+                    return null;}
+                if (Objects.equals(message, "Bankrupt")){
+                    this.playGamePresenter.Bankrupt();
+                    return "Bankrupt";
+                }
+                else{
+                    this.playGamePresenter.Mortgage(message);
+                    return null;
+                }
+            }
+
+            else if (tile == "Go to Jail") {
+                this.playerGoToJailProcessor.PlayerGoToJail();
+                this.playGamePresenter.PlayerGoToJail();
+                return null;
+            }
+
+            else if (tile == "Chance Card"){
+                String message = this.deckDealToPlayer.DealChanceCard();
+                if (Objects.equals(message, "Free card")){
+                    this.playGamePresenter.getOutofJailFreeCard();
+                }
+                else{
+                    //this.playerGoToJailProcessor.PlayerGoToJail();
+                    this.playGamePresenter.PlayerGoToJail();
+                }
+                return null;
+            }
+            else{
+                String message = this.deckDealToPlayer.DealCommunityCard();
+                if (Objects.equals(message, "Go")){
+                    this.playGamePresenter.CommunityCardPresenter("Advance to Go. (Collect $200)");
+                    return null;
+                }
+                else if (Objects.equals(message, "Collect")){
+                    this.playGamePresenter.CommunityCardPresenter("Bank error in your favor. Collect $200.");
+                    return null;
+                }
+                else if (Objects.equals(message, "Bankrupt")){
+                    this.playGamePresenter.Bankrupt();
+                    return message;
+                }
+                else if (Objects.equals(message, "Doctor")){
+                    this.playGamePresenter.CommunityCardPresenter("Doctor's fees. {fee} Pay $50.");
+                    return null;
+                }
+                else if (Objects.equals(message, "Free card")){
+                    this.playGamePresenter.getOutofJailFreeCard();
+                    return null;
+                }
+                else{
+                    this.playerGoToJailProcessor.PlayerGoToJail();
+                    this.playGamePresenter.PlayerGoToJail();
+                    return null;
+                }
+
+            }
+        }
+        else if(tile instanceof TileCanBuy) {
+            if (Objects.equals(this.movePlayer.getPropertyOwner((TileCanBuy) tile),null)){
+                // call UI and wait UI to pass true or false.
+                ArrayList<Object> buy = new ArrayList<>(1);
+                buy.add(null);
+                this.playGamePresenter.BuyProperty(buy, this.userTileInteractor.getName());
+                while (buy.get(0) == null){
+                    System.out.println("");
+                }
+
+                if (Objects.equals(buy.get(0), true)){
+                    String message = this.userTileInteractor.buy();
+                    if (Objects.equals(message, "Success")){
+                        this.playGamePresenter.BuyPropertySuccessful(this.userTileInteractor.getName());
+                    }
+                    else{
+                        this.playGamePresenter.NotEnoughMoney();
+                    }
+                }
+                return null;
+            }
+            else{
+                String message = this.userTileInteractor.payRent(this.rolls);
+                if (Objects.equals(message, "Bankrupt")) {
+                    this.playGamePresenter.Bankrupt();
+                    return "Bankrupt";
+                }
+                if (message.contains("Paid Rent to")){
+                    this.playGamePresenter.payRent(this.userTileInteractor.getRent(rolls), userTileInteractor.getName(), false, null);
+                }
+                else {
+                    this.playGamePresenter.payRent(this.userTileInteractor.getRent(rolls), userTileInteractor.getName(), true, message);
+                    this.playGamePresenter.Mortgage(message);
+                }
+                return null;
+            }
+        }
+        return null;
+
+    }
+
+
+}
