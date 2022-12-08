6fa77204ae4875d0bf37dda04c58691e4f37f6d9
adamjvalentini <113124536+adamjvalentini@users.noreply.github.com>
2022-12-08 11:00:00 -0500

    Update PlayerFactory.java
    Added Javadocs

--- src/main/java/UseCase/PlayerFactory.java
+++ src/main/java/UseCase/PlayerFactory.java
@@ -3,6 +3,9 @@ package UseCase;
 import Entity.Player;
 import Entity.GameBoard;
 
+/**
+ * Class to create and add Players to the GameBaord 
+ */
 public class PlayerFactory {
 
     private final GameBoard gameBoard;
@@ -11,6 +14,11 @@ public class PlayerFactory {
         this.gameBoard = gb;
     }
 
+    /**
+     * Create a Player object based on the inputted id and name
+     * @param id the id of the player 
+     * @param name the name of the Player 
+     */
     public void createPlayer(int id, String name) {
 
         Player player = new Player(name, id);
