c23ee2c2cfc15d1c40c12df4597a27d7d4a7989a
Damngoblin <113082291+Damngoblin@users.noreply.github.com>
2022-12-08 14:33:25 -0500

    Update Jail.java

--- src/main/java/Entity/Jail.java
+++ src/main/java/Entity/Jail.java
@@ -1,82 +1,47 @@
 package Entity;
-// How to go to Jail:
-// roll double thrice consecutively
-// go to jail chance card (if passed go to land on card chest, collect go money, else go directly to jail)
-// land on jail (visiting, no penalty)
 
 import java.util.ArrayList;
 
 public class Jail {
-    Player player;
-    private int turnsInJail;
-    private boolean inJail;
+    // How to go to Jail:
+    // roll double thrice consecutively
+    // go to jail chance card (if passed go to land on card chest, collect go money, else go directly to jail)
+    // land on jail (visiting, no penalty)
+    private ArrayList<Player> players;
 
     /**
      * Initialize Jail Object
      *
-     * 
      */
-    
+
     public Jail() {
         this.players = new ArrayList<>();
     }
 
     /**
-     * While suspended, Player can attempt to roll doubles to get out
+     * Getter method to get all players currently in Jail
      *
-     * While in Jail, Player cannot move from their position
-     * Their turn is suspended up to 3 turns, they can attempt to bail, or roll to get out
-     * Player can still conduct activities such as collect rent, auction, mortgage, etc
      */
-    public void turnSuspended() {
-        ArrayList<Integer> dice = player.rollDice();
-        if ((int) dice.get(1) == (int) dice.get(2)) {
-            this.outOfJail();
-        }
+    public ArrayList<Player> getPlayers(){
+        return this.players;
     }
-
     /**
-     * Player moves out of jail, the instance variables resets
-     *
-     * Player moves out of jail after rolling doubles (Player moves amount thrown, double rule do not apply)
-     * or by paying bail (see payBail)
-     */
-    public void outOfJail() {
-        this.inJail = false;
-        turnsInJail = 0;
-    }
-
-    /**
-     * Player pays $50 to the bank
-     *
-     * Player can pay bail either before first or second turn
-     * Player must pay bail at third turn
-     */
-    public void payBail() {
-        player.loseMoney(50); // lose to the bank
-    }
-
-    /**
-     * The followings are all getters and setters for the instance variable
-     * @return instance variables
-     */
-    public int getTurnsInJail() {
-        return turnsInJail;
-    }
-    
-    /**
-     * Remove player from the jail
-     * @param player
-     */
-    public void removePlayer(Player player) {
-        this.players.remove(player);
-    }
-    
-    /**
-     * Set player to the jail
-     * @param player
+     * set player to jail
+     * @param player: a player
      */
     public void addPlayer(Player player) {
         this.players.add(player);
     }
+
+    /**
+     * remove player from jail
+     * @param player: a player
+     */
+    public void removePlayer(Player player) {
+        this.players.remove(player);
+    }
+
+
+
+
 }
