cdf92b59951ee6dd2197473e92f1f8da9a473fce
adamjvalentini <113124536+adamjvalentini@users.noreply.github.com>
2022-12-08 12:01:17 -0500

    Update OpportunityCard.java
    Added javadocs

--- src/main/java/Entity/OpportunityCard.java
+++ src/main/java/Entity/OpportunityCard.java
@@ -7,11 +7,28 @@ abstract class OpportunityCard {
     int moneychange;
     int movement;
 
+    /**
+     * Instantiate an Opportunity card object. This is an abstarct class
+     */
     public OpportunityCard() {}
 
+    /**
+     * Abstarct parent class 
+     * @return card 
+     */
     abstract String getCard();
     //will be implemented in child class
 
+    /**
+     * Abstract method 
+     * @return int 
+     */
     abstract int getMovement();
+
+    /**
+     * Abstract method
+     * 
+     * @return int 
+     */
     abstract int getMoneychange();
 }
