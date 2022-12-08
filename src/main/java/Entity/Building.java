98eac15e31bf5a487c98769d0d4051dc9e9ff353
adamjvalentini <113124536+adamjvalentini@users.noreply.github.com>
2022-12-08 11:49:41 -0500

    Update Building.java
    Added Javadocs

--- src/main/java/Entity/Building.java
+++ src/main/java/Entity/Building.java
@@ -1,5 +1,8 @@
 package Entity;
 
+/**
+ * Abstract class to be extended by the House and Hotel Classes 
+ */
 public abstract class Building{
     // Abstract class Building
 
@@ -8,16 +11,33 @@ public abstract class Building{
     private Integer mortgageValue;
 
 
-
+    /**
+     * Instantiate a building object 
+     * @param price the price of the Building 
+     * @param rent the rent of the Building 
+     * @param mortgageValue the mortgageValue of the Building 
+     */
     public Building(Integer price, Integer rent, Integer mortgageValue) {
         this.price = price;
         this.rent = rent;
         this.mortgageValue =  mortgageValue;
     }
 
+    /**
+     * Getter method for the price 
+     * @return the price of the building 
+     */
     public int getPrice(){return this.price;}
 
+    /**
+     * Getter method for the rent 
+     * @return the rent of the building 
+     */
     public int getRent(){return this.rent;}
 
+    /**
+     * Getter method for the mortgageValue 
+     * @return the mortgageValue of the building 
+     */
     public int getMortgageValue(){return this.mortgageValue;}
 }
