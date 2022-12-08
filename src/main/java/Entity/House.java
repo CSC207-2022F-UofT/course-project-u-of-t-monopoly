41ee2a251f69308e3583316c1573f8cc0c6682a6
adamjvalentini <113124536+adamjvalentini@users.noreply.github.com>
2022-12-08 11:54:13 -0500

    Update House.java
    Added Javadocs

--- src/main/java/Entity/House.java
+++ src/main/java/Entity/House.java
@@ -3,6 +3,12 @@ package Entity;
 public class House extends Building{
     //subclass of Building
 
+    /**
+     * Instantiate a House Object
+     * @param price the price of the House 
+     * @param rent the Rent of the House 
+     * @param mortgageValue the mortgageValue of the House 
+     */
     public House(Integer price, Integer rent, Integer mortgageValue){
         super(price, rent, mortgageValue);
     }
