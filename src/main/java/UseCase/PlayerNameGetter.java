64b082db01074a8e493e1424cbaa532272abb103
adamjvalentini <113124536+adamjvalentini@users.noreply.github.com>
2022-12-08 11:10:13 -0500

    Update PlayerNameGetter.java
    Added Javadocs

--- src/main/java/UseCase/PlayerNameGetter.java
+++ src/main/java/UseCase/PlayerNameGetter.java
@@ -2,9 +2,16 @@ package UseCase;
 
 import Entity.Player;
 
+/**
+ * Class for returning the name of a Player object as a use case rather than at the Entity level 
+ */
 public class PlayerNameGetter {
 
-
+    /**
+     * return the name of the Player object 
+     * @param player the Player object who's name is needed
+     * @return the name of the Player object 
+     */
     public String getName(Player player) {
         return player.getUsername();
     }
