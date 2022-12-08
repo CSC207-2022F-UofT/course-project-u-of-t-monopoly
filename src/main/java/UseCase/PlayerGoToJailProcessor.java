2ac542a44e102d5f250c3a3034559be99d71c5b2
adamjvalentini <113124536+adamjvalentini@users.noreply.github.com>
2022-12-08 11:02:49 -0500

    Update PlayerGoToJailProcessor.java
    Added Javadocs

--- src/main/java/UseCase/PlayerGoToJailProcessor.java
+++ src/main/java/UseCase/PlayerGoToJailProcessor.java
@@ -3,17 +3,29 @@ package UseCase;
 import Entity.Player;
 import Entity.GameBoard;
 
+/**
+ * A use case class to set, check, and update the Players position in jail 
+ */
 public class PlayerGoToJailProcessor{
 
     private Player player;
     private GameBoard gameboard;
 
+    /**
+     * Instantiate a new PlayerGoToJailProcessor 
+     * @param player the Player object to be updated 
+     * @param gameboard the GameBoard object associated with the game 
+     */
     public PlayerGoToJailProcessor(Player player, GameBoard gameboard){
         this.player = player;
         this.gameboard = gameboard;
 
     }
 
+    /**
+     * Update the Players position in jail
+     * @return true if the Player was set to jail, false otherwise 
+     */
     public boolean PlayerGoToJail(){
         if(this.player.getPlayerPosition() == 30) {
             if(this.player.isInJail() == false ){
